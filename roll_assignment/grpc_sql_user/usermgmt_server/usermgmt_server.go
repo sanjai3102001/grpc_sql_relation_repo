feb940e8f67ad161880ceb0cb74fb1d301ea86e6
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2023-03-15 15:53:11 +0530

    pushing the service in roll assaignment

--- roll_assignment/grpc_sql_user/usermgmt_server/usermgmt_server.go
+++ roll_assignment/grpc_sql_user/usermgmt_server/usermgmt_server.go
@@ -64,14 +64,14 @@ func (server *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.Ne
 
 	log.Printf("Received: %v %v %v %v %v %v %v", in.GetAssignmentId(), in.GetAssaignee(), in.GetRollId(), in.GetStatus(), in.GetTenantId(), in.GetServiceId(), in.GetIsGroup())
 
-	created_user := &pb.User{RollId: in.GetRollId(), Name: in.GetName(), Statement: in.GetStatement(), OwnerServiceId: in.GetOwnerServiceId(), ServiceId: in.GetServiceId()}
+	created_user := &pb.User{AssignmentId: in.GetAssignmentId(), Assaignee: in.GetAssaignee(), RollId: in.GetRollId(), Status: in.GetStatus(), TenantId: in.GetTenantId(), ServiceId: in.GetServiceId(), IsGroup: in.GetIsGroup()}
 	tx, err := server.conn.Begin(context.Background())
 	if err != nil {
 		log.Fatalf("conn.Begin failed: %v", err)
 	}
 
-	_, err = tx.Exec(context.Background(), "insert into roles(roll_id, name, statement, owner_service_id, service_id) values ($1,$2,$3,$4,$5)",
-		created_user.RollId, created_user.Name, created_user.Statement, created_user.OwnerServiceId, created_user.ServiceId)
+	_, err = tx.Exec(context.Background(), "insert into roll_assignment(assignment, assaignee, roll_id, status, tenant_id, service_id) values ($1,$2,$3,$4,$5,$6,$7)",
+		created_user.AssignmentId, created_user.Assaignee, created_user.RollId, created_user.Status, created_user.TenantId, created_user.ServiceId, created_user.IsGroup)
 	if err != nil {
 		log.Fatalf("tx.Exec failed: %v", err)
 	}
@@ -83,14 +83,14 @@ func (server *UserManagementServer) CreateNewUser(ctx context.Context, in *pb.Ne
 func (server *UserManagementServer) GetUsers(ctx context.Context, in *pb.GetUsersParams) (*pb.UsersList, error) {
 
 	var users_list *pb.UsersList = &pb.UsersList{}
-	rows, err := server.conn.Query(context.Background(), "select * from roles")
+	rows, err := server.conn.Query(context.Background(), "select * from roll_assignment")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		user := pb.User{}
-		err = rows.Scan(&user.ServiceId, &user.OwnerServiceId, &user.Statement, &user.Name, &user.RollId)
+		err = rows.Scan(&user.IsGroup, &user.ServiceId, &user.TenantId, &user.Status, &user.RollId, &user.Assaignee, &user.AssignmentId)
 		if err != nil {
 			return nil, err
 		}
